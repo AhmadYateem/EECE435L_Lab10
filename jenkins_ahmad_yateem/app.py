cf678c112a424863de7c284eea91e96411701b65
AhmadYateem <aay26@mail.aub.edu>
2025-11-10 02:55:32 +0200

    test(coverage): exclude tests from coverage and ignore __main__ lines

--- jenkins_ahmad_yateem/app.py
+++ jenkins_ahmad_yateem/app.py
@@ -2,5 +2,5 @@ def greet(name):
     return f"Hello, {name} from Ahmad Yateem!"
 
 
-if __name__ == "__main__":
-    print(greet("World"))
+if __name__ == "__main__":  # pragma: no cover
+    print(greet("World"))  # pragma: no cover
